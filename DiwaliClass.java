1a04c0953250bc97c836a60c0a1478cdcac90d83
Xworkz-Rakshitha MMJ <rakshi.xworkz@gmail.com>
2020-11-19 08:20:28 +0530

    Adding methods files

--- DiwaliClass.java
+++
@@ -1,9 +0,0 @@
-class DiwaliClass
-{
-	public static void main(String a[])
-	{
-		Diwali.pooja();
-		Diwali.burningCrackers();
-		Diwali.makingSweetsAtHome();
-	}
-}
--- WhatsAppTester.java
+++
@@ -1,9 +0,0 @@
-class WhatsAppTester
-{
-	public static void main(String a[])
-	{
-		WhatsApp.Chats();
-		WhatsApp.Status();
-		WhatsApp.Calls();
-	}
-}
