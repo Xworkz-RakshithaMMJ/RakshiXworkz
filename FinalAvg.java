eb6eccb6701901f59d09f837c4dd423394ce63aa
Xworkz-Rakshitha MMJ <rakshi.xworkz@gmail.com>
2020-11-29 18:18:24 +0530

    adding java files

--- FinalAvg.java
+++
@@ -1,9 +0,0 @@
-class FinalAvg
-{
-	
-	static double avg(double firstSubject ,double secondSubject ,double thirdSubject)
-	{
-		double total=(firstSubject + secondSubject + thirdSubject)/3;
-		return total;
-	}
-}
