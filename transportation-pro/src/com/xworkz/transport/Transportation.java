e07f499ec3ef60dce50bc674bc39924665b73286
Xworkz-Rakshitha MMJ <rakshi.xworkz@gmail.com>
2020-12-05 22:14:49 +0530

    commiting file

---
+++ transportation-pro/src/com/xworkz/transport/Transportation.java
@@ -0,0 +1,19 @@
+package com.xworkz.transport;
+
+public class Transportation {
+	public String id;
+	public String name;
+	public String address[]= new String[5];//initialising an array using new keyword
+	public int noOfBranches;
+	
+	
+public	void importAndExport()
+	{
+		System.out.println("Exporting from bengaluru and importing to ballari,mysuru");
+	}
+	
+public	void transport()
+	{
+		System.out.println("vacation to manali");
+	}
+}
