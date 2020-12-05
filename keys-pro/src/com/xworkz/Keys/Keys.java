a2aee27ff2fb22943212a67f1d8f6efc410bfb2f
Xworkz-Rakshitha MMJ <rakshi.xworkz@gmail.com>
2020-12-05 23:17:34 +0530

    Commiting files

---
+++ keys-pro/src/com/xworkz/Keys/Keys.java
@@ -0,0 +1,16 @@
+package com.xworkz.Keys;
+
+public class Keys {
+	public double price;
+	public int noOfKeys;
+	public static void toLock()
+	{
+		System.out.println("Key is used to lock");
+	}
+	public static void toUnlock()
+	{
+		System.out.println("Key is used to Unlock");
+	
+	}
+
+}
