d92c007bc0034e1f712f0324fdffd8338d6496ce
Xworkz-Rakshitha MMJ <rakshi.xworkz@gmail.com>
2020-12-05 14:17:39 +0530

    commiting eclipse file

---
+++ music-pro/src/com/xworkz/Music.java
@@ -0,0 +1,15 @@
+package com.xworkz;
+
+public class Music {
+	
+	String name;   //instance variables
+	String categories;
+	String musicDirector;
+	
+	
+	void relax()
+	{
+		System.out.println("relaxation");
+	}
+	
+}
---
+++ music-pro/src/com/xworkz/MusicUtil.java
@@ -0,0 +1,14 @@
+package com.xworkz;
+
+public class MusicUtil {
+
+	public static void main(String[] args) {
+		
+		Music music = new Music();
+		music.name="melody";
+		music.musicDirector="dev";
+		music.categories="soothing";
+		System.out.println(music.name + " "+music.musicDirector +" "+music.categories);
+	}
+
+}
