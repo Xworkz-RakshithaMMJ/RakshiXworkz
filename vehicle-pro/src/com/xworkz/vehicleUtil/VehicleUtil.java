eef1950db1a36792c5612c8f700553cdf3df02d9
Xworkz-Rakshitha MMJ <rakshi.xworkz@gmail.com>
2020-12-08 20:03:32 +0530

    Done using Constructor initialization

--- vehicle-pro/src/com/xworkz/vehicleUtil/VehicleUtil.java
+++ vehicle-pro/src/com/xworkz/vehicleUtil/VehicleUtil.java
@@ -1,8 +1,8 @@
-package com.xworkz;
+package com.xworkz.vehicleUtil;
 
 import com.xworkz.vehicle.Vehicle;
 
-public class vehicleUtil {
+public class VehicleUtil {
 public static void main(String[] args) {
 	Vehicle vehicle = new Vehicle("Wego","Scooty","Two wheeler",50000.0);
 	System.out.println(vehicle.getName()+" "+vehicle.getTypeOfVehicle()+" "+vehicle.getNoOfWheels()+" "+vehicle.getPrice());
